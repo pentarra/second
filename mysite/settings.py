73c10a1c626fb7f571ea6c2a42f1d4382afe1254
pentarra <pentarra@gmail.com>
2020-08-27 23:20:21 +0300

    add host

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'cw$i(q^72hk0jds$ftec+@=^9p_lph9(!ty6xfl6&%g-s-_usr'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
