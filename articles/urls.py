22c3421bc507f67853303e2880b7186cf73d7e04
pentarra <pentarra@gmail.com>
2020-08-28 04:59:49 +0300

    add buttons

--- articles/urls.py
+++ articles/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
 		path('', views.index, name ='index'),
 		path('<int:article_id>/', views.detail, name ='detail'),
 		path('<int:article_id>/leave_comment/', views.leave_comment, name ='leave_comment'),
+		path('post/new/', views.post_new, name='post_new'),
+		path('<int:article_id>/edit/', views.post_edit, name='post_edit'),
 	]
--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from .models import Article
+from .forms import ArticleForm
+from django.utils import timezone
 
 def index(request):
 	latest_articles_list = Article.objects.order_by('-pub_date')[:5]
@@ -23,4 +25,30 @@ def leave_comment(request, article_id):
 	a.comment_set.create(author_name = request.POST['name'], comment_text = request.POST['text'])
 	return HttpResponseRedirect(reverse('articles:detail', args  = (a.id, )))
 
+def post_new(request):
+    if request.method == "POST":
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('detail', article_id=post.id)
+    else:
+        form = ArticleForm()
+    return render(request, 'articles/post_edit.html', {'form': form})
+
+def post_edit(request, article_id):
+    post = get_object_or_404(Article, id=article_id)
+    if request.method == "POST":
+        form = ArticleForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('articles:detail', article_id=post.id)
+    else:
+        form = ArticleForm(instance=post)
+    return render(request, 'articles/post_edit.html', {'form': form})
 
